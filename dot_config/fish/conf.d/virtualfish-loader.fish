c7e3924be71934a1e7288e882b32ac3f9b51dab0
Kashim Aziz <kashim_aziz@yahoo.co.uk>
2024-06-15 00:37:35 +0100

    Remove unneeded fish config

--- dot_config/fish/conf.d/virtualfish-loader.fish
+++
@@ -1,4 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.5
-set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.11/bin/python3.11
-source /Users/kash/Library/Python/3.11/lib/python/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
