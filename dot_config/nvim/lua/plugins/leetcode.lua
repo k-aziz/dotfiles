5817061df70bd0693d034f6cea98e0f3f199c7f6
Kashim Aziz <kashim_aziz@yahoo.co.uk>
2024-08-06 00:51:00 +0100

    Add leetcode + plugin updates

---
+++ dot_config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,24 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- configuration goes here
+    lang = "python3",
+    theme = {
+      normal = {
+        fg = "#8c605d",
+        -- fg = "#5e8edb",
+      },
+    },
+  },
+}
